4a39cf0756eb7e76f9cebf9aeb5e767f774da48d
Spiro Douvis <spiro.douvis@ucalgary.ca>
2025-04-02 19:13:43 -0600

    Added additional functions according to MSC File specifications

--- MX66L1G45GMI/Inc/MX66L1G45GMI.hpp
+++ MX66L1G45GMI/Inc/MX66L1G45GMI.hpp
@@ -29,7 +29,7 @@
 /************************************
  * FUNCTION DECLARATIONS
  ************************************/
-uint32_t MX66_ReadID (void);
+uint32_t MX66_ReadID(void);
 
 uint8_t MX66_ReadStatus(int reg);								// Read status reg1,2,3
 
@@ -37,9 +37,9 @@ void MX66_WriteStatus(int reg, uint8_t newstatus);
 
 void MX66_ReadSFDP(uint8_t *rData);
 
-void MX66_Read (uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData);
+void MX66_Read(uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData);
 
-void MX66_FastRead (uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData);
+void MX66_FastRead(uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData);
 
 void write_enable(void);
 
@@ -51,7 +51,11 @@ void MX66_Erase_Sector(uint16_t numsector);
 
 void MX66_Erase_Block(uint32_t numblock);
 
+void MX66_Write_Page(uint32_t page, uint16_t offset, uint32_t size, const uint8_t *data);
 
+void MX66_Write_Block(uint32_t block, uint16_t offset, uint32_t size, const uint8_t *data);
+
+bool isWriteProtected(void);
 
 
 
--- MX66L1G45GMI/MX66L1G45GMI.cpp
+++ MX66L1G45GMI/MX66L1G45GMI.cpp
@@ -53,7 +53,7 @@ void SPI_Read (uint8_t *data, uint16_t len)
 /************************************
  * FUNCTION DEFINITIONS
  ************************************/
-uint32_t MX66_ReadID (void)
+uint32_t MX66_ReadID(void)
 {
 	uint8_t tData = 0x9F;
 	uint8_t rData[3];
@@ -66,13 +66,13 @@ uint32_t MX66_ReadID (void)
 }
 
 
-uint8_t MX66_ReadStatus(int reg) // WIP Bit???
+uint8_t MX66_ReadStatus(int reg)
 {
 	uint8_t tData,rData;
 	switch(reg){
 		case 1: tData = 0x05; break;
-		case 2: tData = 0x35; break;
-		case 3: tData = 0x15; break;
+//		case 2: tData = 0x35; break;
+//		case 3: tData = 0x15; break;
 		default:
 			printf("Invalid status register 0\n");
 			return 0;
@@ -89,8 +89,8 @@ void MX66_WriteStatus(int reg, uint8_t newstatus)
 	uint8_t tData[2];
 	switch(reg){
 		case 1: tData[0] = 0x01; break;
-		case 2: tData[0] = 0x31; break;  // Cant understand cases
-		case 3: tData[0] = 0x11; break;
+//		case 2: tData[0] = 0x31; break;
+//		case 3: tData[0] = 0x11; break;
 		default: return;
 	}
 
@@ -112,7 +112,7 @@ void W25Q_ReadSFDP(uint8_t *rData)
 }
 
 
-void MX66_Read (uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData)
+void MX66_Read(uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData)
 {
 	uint8_t tData[6];
 	uint32_t memAddr = (block*FS_SECTOR_SIZE) + offset;
@@ -129,7 +129,7 @@ void MX66_Read (uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData)
 }
 
 
-void MX66_FastRead (uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData)
+void MX66_FastRead(uint32_t block, uint16_t offset, uint32_t size, uint8_t *rData)
 {
 	uint8_t tData[6];
 	uint32_t memAddr = (block*FS_SECTOR_SIZE) + offset;
@@ -220,3 +220,70 @@ void MX66_Erase_Block(uint32_t block)
 	write_disable();
 }
 
+void MX66_Write_Page(uint32_t page, uint16_t offset, uint32_t size, const uint8_t *data)
+{
+	uint8_t tData[266];
+	uint32_t memAddr = (page*FS_PAGE_SIZE)+offset;
+	uint32_t indx = 0;
+
+	dprintf("W25Q_Write(page=%ld, offset=%d, memaddr=%ld) memAddr=%08lx\n",page,offset,size,memAddr);
+
+	write_enable();
+
+	tData[0] = 0x02;  												// block program
+	tData[1] = (memAddr>>16)&0xFF;  								// MSB of the memory Address
+	tData[2] = (memAddr>>8)&0xFF;
+	tData[3] = (memAddr)&0xFF; 										// LSB of the memory Address
+	indx = 4;
+
+	memcpy(&tData[indx],data,size);
+
+	csLOW();
+	SPI_Write(tData, indx+size);
+	csHIGH();
+
+	while(W25Q_ReadStatus(1)&0x01);									// Check BUSY is low, if not wait, TODO add timeout?
+	write_disable();
+}
+
+void MX66_Write_Block(uint32_t block, uint16_t offset, uint32_t size, const uint8_t *data)
+{
+	uint32_t startpage=((block*FS_SECTOR_SIZE)+offset)/FS_PAGE_SIZE;
+	uint32_t bytesleft=size;
+	uint32_t newoff=offset%256;
+	uint32_t bufptr=0;
+
+	dprintf("W25Q_Write_block(%ld,%d,%ld)  startpage=%ld newoff=%ld\n",block,offset,size,startpage,newoff);
+
+	dprintf("First %ld,%04ld,%03ld ",startpage,newoff,(FS_PAGE_SIZE-newoff));
+	MX66_Write_Page(startpage, newoff, (FS_PAGE_SIZE-newoff), data);				// First block
+	bufptr=(FS_PAGE_SIZE-newoff);
+	bytesleft-=(FS_PAGE_SIZE-newoff);
+
+	startpage++;
+	while (bytesleft) {
+
+		if (bytesleft>256) {
+			dprintf("Page %ld,%04d,%03d ",startpage,0,FS_PAGE_SIZE);
+			Write_page(startpage, 0, FS_PAGE_SIZE, &data[bufptr]);
+			bytesleft-=FS_PAGE_SIZE;
+			bufptr+=FS_PAGE_SIZE;
+		} else {
+			if (newoff) {
+				dprintf("Last  %ld,%04d,%03ld ",startpage,0,newoff);
+				Write_page(startpage, 0, newoff, &data[bufptr]);	// Last block
+			} else {
+				dprintf("Last  %ld,%04d,%03ld ",startpage,0,bytesleft);
+				Write_page(startpage, 0, bytesleft, &data[bufptr]);
+				bufptr+=bytesleft;
+			}
+			bytesleft=0;
+		}
+		startpage++;
+	}
+	dprintf("\n");
+}
+
+bool isWriteProtected(void){
+	return false;
+}
