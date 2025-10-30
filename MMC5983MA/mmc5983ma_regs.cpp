7a7d0a8a5387e4599ef094cf61821207687bd740
Javier <javier.dalmontecason@ucalgary.ca>
2025-10-29 18:53:48 -0600

    Initial commit, MMC5983MA magnetometer project Structure.

---
+++ MMC5983MA/mmc5983ma_regs.cpp
@@ -0,0 +1,9 @@
+/*
+ * nau7802_regs.hpp
+ *
+ * Register definitions and constants for the MMC5983MA magnetometer.
+ */
+#ifndef mmc5983ma_regs_HPP
+#define mmc5983ma_regs_HPP
+
+#endif // mmc5983ma_regs_HPP
