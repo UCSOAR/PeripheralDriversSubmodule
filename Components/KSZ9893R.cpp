bc07e1df012db74ed89258321f2b99d3a00c39fb
Kailey <kaileykobar@gmail.com>
2026-01-15 16:09:07 -0700

    fixed KSZ9893R.hpp and added read/write register and other needed functions

--- Components/KSZ9893R.cpp
+++ Components/KSZ9893R.cpp
@@ -7,3 +7,88 @@
 
 #define SPI_DUMMY_BYTE 0xFF
 
+
+void KSZ9893RDriver::Init(SPI_HandleTypeDef* hspi_, GPIO_TypeDef* gpio, uint16_t pin) {
+    hspi = hspi_;
+    cs_gpio = gpio;
+    cs_pin = pin;
+    initialized = true;
+
+    CSHigh();
+
+    // Verify chip ID
+    uint8_t id1 = ReadRegister(KSZ9893R_REG::CHIP_ID1);
+    uint8_t id2 = ReadRegister(KSZ9893R_REG::CHIP_ID2);
+
+    if(id1 != KSZ9893R_CHIP_ID1_VAL || id2 != KSZ9893R_CHIP_ID2_VAL)
+        initialized = false;
+}
+
+bool KSZ9893RDriver::WriteRegister(uint16_t addr, uint8_t val) {
+    assert(initialized);
+
+    uint8_t tx[4];
+    tx[0] = KSZ_SPI_WRITE | ((addr >> 9) & 0x7F);
+    tx[1] = (addr >> 1) & 0xFF;
+    tx[2] = (addr & 0x01) << 7;
+    tx[3] = val;
+
+    CSLow();
+    HAL_StatusTypeDef status = HAL_SPI_Transmit(hspi, tx, 4, HAL_MAX_DELAY);
+    CSHigh();
+
+    return status == HAL_OK;
+}
+
+uint8_t KSZ9893RDriver::ReadRegister(uint16_t addr) {
+    assert(initialized);
+
+    uint8_t tx[4];
+    uint8_t rx[4] = {0};
+
+    tx[0] = KSZ_SPI_READ | ((addr >> 9) & 0x7F);
+    tx[1] = (addr >> 1) & 0xFF;
+    tx[2] = (addr & 0x01) << 7;
+    tx[3] = SPI_DUMMY_BYTE;
+
+    CSLow();
+    HAL_SPI_TransmitReceive(hspi, tx, rx, 4, HAL_MAX_DELAY);
+    CSHigh();
+
+    return rx[3];
+}
+
+void KSZ9893RDriver::ReadMultipleRegisters(uint16_t startreg, int numBytes, uint8_t *out) {
+    assert(initialized);
+    for(int i = 0; i < numBytes; i++) {
+        out[i] = ReadRegister(startreg + i);
+    }
+}
+
+void KSZ9893RDriver::CSLow() {
+    assert(initialized);
+    HAL_GPIO_WritePin(cs_gpio, cs_pin, GPIO_PIN_RESET);
+}
+
+void KSZ9893RDriver::CSHigh() {
+    assert(initialized);
+    HAL_GPIO_WritePin(cs_gpio, cs_pin, GPIO_PIN_SET);
+}
+
+void KSZ9893RDriver::SetCSPin(GPIO_TypeDef* gpio, uint16_t pin) {
+    cs_gpio = gpio;
+    cs_pin = pin;
+}
+
+
+bool KSZ9893RDriver::SoftReset() {
+    uint8_t reg = ReadRegister(SWITCH_OP_REG);
+    reg |= 0x01;
+    return WriteRegister(SWITCH_OP_REG, reg);
+}
+
+bool KSZ9893RDriver::LinkStatus(uint8_t port) {
+    uint8_t status = ReadRegister(PHY_STATUS_BASE + port);
+    return (status & LINK_UP_BIT) != 0;
+}
+
--- Components/KSZ9893R.hpp
+++ Components/KSZ9893R.hpp
@@ -4,37 +4,46 @@
 #include "stm32l4xx_hal.h"
 #include <stdint.h>
 
-namespace KSZ9893R_REGISTER {
-    enum REGISTER : uint16_t {
-        CHIP_ID1 = 0x0001,
-        CHIP_ID2 = 0x0002
+namespace KSZ9893R_REG {
+    enum REG : uint16_t {
+        CHIP_ID1    = 0x0000,
+        CHIP_ID2    = 0x0001,
+        SWITCH_OP   = 0x0300,
+
+        PHY1_STATUS = 0x0100,
+        PHY2_STATUS = 0x0101,
+        PHY3_STATUS = 0x0102
     };
 }
-//chip IDs
+
 #define KSZ9893R_CHIP_ID1_VAL 0x98
 #define KSZ9893R_CHIP_ID2_VAL 0x93
 
 class KSZ9893RDriver {
 public:
-        KSZ9893RDriver(SPI_HandleTypeDef* hspi,
-            GPIO_TypeDef* cs_gpio,
-            uint16_t cs_pin)
-        : hspi(hspi), cs_gpio(cs_gpio), cs_pin(cs_pin) {}
+    KSZ9893RDriver();
+    ~KSZ9893RDriver();
 
-    uint8_t ReadReg(uint16_t reg);
-    void WriteReg(uint16_t reg, uint8_t val);
+    void Init(SPI_HandleTypeDef *hspi, GPIO_TypeDef *gpio, uint16_t pin);
+
+    bool SetRegister(KSZ9893R_REG::REG reg, uint8_t val);
+    uint8_t GetRegister(KSZ9893R_REG::REG reg);
+    void GetMultipleRegisters(KSZ9893R_REG::REG startreg,
+                              int numBytes,
+                              uint8_t *out);
+
+    bool SoftReset();
+    bool LinkStatus(uint8_t port);
 
 private:
-    SPI_HandleTypeDef* hspi;
-    GPIO_TypeDef* cs_gpio;
-    uint16_t cs_pin;
+    SPI_HandleTypeDef *hspi = nullptr;
+    GPIO_TypeDef *cs_gpio = nullptr;
+    uint16_t cs_pin = 0;
+    bool initialized = false;
 
-    void CSLow() { HAL_GPIO_WritePin(cs_gpio, cs_pin, GPIO_PIN_RESET); }
-    void CSHigh() { HAL_GPIO_WritePin(cs_gpio, cs_pin, GPIO_PIN_SET); }
-    
-    coid SPI_TxRx(uint8_t* tx, uint8_t* rx, uint16_t len) {
-        HAL_SPI_TransmitReceive(hspi, tx, rx, len, HAL_MAX_DELAY);
-    }
+    void CSLow();
+    void CSHigh();
+    void SetCSPin(GPIO_TypeDef *gpio, uint16_t pin);
 };
 
 #endif
