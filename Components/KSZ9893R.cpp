831e96880e4d22d2a90bad2c2860a9dd07bfd7ad
Kailey <kaileykobar@gmail.com>
2026-01-17 12:01:00 -0700

    Fix KSZ9893R driver .hpp/.cpp mismatch and RMII clock mode on port 3

--- Components/KSZ9893R.cpp
+++ Components/KSZ9893R.cpp
@@ -2,11 +2,10 @@
 
 #define KSZ_SPI_READ   0x80
 #define KSZ_SPI_WRITE  0x00
-#define SPI_DUMMY      0xFF
+#define KSZ_SPI_AI     0x40
 
 #define KSZ9893R_CHIP_ID1_VAL 0x98
 #define KSZ9893R_CHIP_ID2_VAL 0x93
-
 
 void KSZ9893RDriver::CSLow() {
     HAL_GPIO_WritePin(cs_gpio, cs_pin, GPIO_PIN_RESET);
@@ -16,125 +15,112 @@ void KSZ9893RDriver::CSHigh() {
     HAL_GPIO_WritePin(cs_gpio, cs_pin, GPIO_PIN_SET);
 }
 
+bool KSZ9893RDriver::SPI_Read(uint16_t addr, uint8_t* data, uint16_t len) {
+    if (!hspi || !data || len == 0)
+        return false;
+
+    uint8_t header[3];
+    header[0] = KSZ_SPI_READ | KSZ_SPI_AI | ((addr >> 10) & 0x3F);
+    header[1] = (addr >> 2) & 0xFF;
+    header[2] = (addr & 0x03) << 6;
+
+    CSLow();
+    if (HAL_SPI_Transmit(hspi, header, 3, HAL_MAX_DELAY) != HAL_OK ||
+        HAL_SPI_Receive(hspi, data, len, HAL_MAX_DELAY) != HAL_OK) {
+        CSHigh();
+        return false;
+    }
+    CSHigh();
+    return true;
+}
+
+bool KSZ9893RDriver::SPI_Write(uint16_t addr, const uint8_t* data, uint16_t len) {
+    if (!hspi || !data || len == 0)
+        return false;
+
+    uint8_t header[3];
+    header[0] = KSZ_SPI_WRITE | KSZ_SPI_AI | ((addr >> 10) & 0x3F);
+    header[1] = (addr >> 2) & 0xFF;
+    header[2] = (addr & 0x03) << 6;
+
+    CSLow();
+    if (HAL_SPI_Transmit(hspi, header, 3, HAL_MAX_DELAY) != HAL_OK ||
+        HAL_SPI_Transmit(hspi, (uint8_t*)data, len, HAL_MAX_DELAY) != HAL_OK) {
+        CSHigh();
+        return false;
+    }
+    CSHigh();
+    return true;
+}
+
+
+uint8_t KSZ9893RDriver::ReadReg(KSZ9893R_REG::REG reg) {
+    uint8_t val = 0;
+    SPI_Read(static_cast<uint16_t>(reg), &val, 1);
+    return val;
+}
+
+bool KSZ9893RDriver::WriteReg(KSZ9893R_REG::REG reg, uint8_t value) {
+    return SPI_Write(static_cast<uint16_t>(reg), &value, 1);
+}
+
+bool KSZ9893RDriver::ReadRegs(uint16_t startAddr, uint8_t* buffer, uint16_t length) {
+    return SPI_Read(startAddr, buffer, length);
+}
+
+bool KSZ9893RDriver::SoftReset() {
+    uint8_t val = ReadReg(KSZ9893R_REG::SWITCH_OP);
+    WriteReg(KSZ9893R_REG::SWITCH_OP, val | 0x01);
+
+    HAL_Delay(10);
+
+    WriteReg(KSZ9893R_REG::SWITCH_OP, val & ~0x01);
+    HAL_Delay(50);
+
+    return true;
+}
+
+bool KSZ9893RDriver::ConfigureRMII_Port3() {
+    WriteReg(KSZ9893R_REG::PORT3_XMII_CTRL, 0x02);
+    WriteReg(KSZ9893R_REG::PORT3_CLK_CTRL,  0x01); 
+    WriteReg(KSZ9893R_REG::PORT3_CTRL_1,    0x01); 
+    return true;
+}
 
 bool KSZ9893RDriver::Init(SPI_HandleTypeDef* hspi_, GPIO_TypeDef* cs_gpio_, uint16_t cs_pin_) {
 
     hspi = hspi_;
     cs_gpio = cs_gpio_;
     cs_pin = cs_pin_;
-    initialized = false;
 
     CSHigh();
-    HAL_Delay(10);
+    HAL_Delay(20);
 
-    uint8_t id1 = GetRegister(KSZ9893R_REG::CHIP_ID1);
-    uint8_t id2 = GetRegister(KSZ9893R_REG::CHIP_ID2);
+    uint8_t id1 = ReadReg(KSZ9893R_REG::CHIP_ID1);
+    uint8_t id2 = ReadReg(KSZ9893R_REG::CHIP_ID2);
 
-    if (id1 == KSZ9893R_CHIP_ID1_VAL &&
-        id2 == KSZ9893R_CHIP_ID2_VAL) {
-        initialized = true;
-        return true;
-    }
-
-    return false;
-}
-
-
-bool KSZ9893RDriver::SPI_Read(uint16_t addr, uint8_t* data, uint16_t len) {
-    if (!initialized || !hspi || !data || len == 0)
+    if (id1 != KSZ9893R_CHIP_ID1_VAL || id2 != KSZ9893R_CHIP_ID2_VAL)
         return false;
 
-    uint8_t header[3];
-    header[0] = KSZ_SPI_READ | ((addr >> 8) & 0x7F);
-    header[1] = addr & 0xFF;
-    header[2] = SPI_DUMMY;
+    SoftReset();
 
-    CSLow();
+    WriteReg(KSZ9893R_REG::PORT1_CTRL_1, 0x01);
+    WriteReg(KSZ9893R_REG::PORT2_CTRL_1, 0x01);
+    WriteReg(KSZ9893R_REG::PORT3_CTRL_1, 0x01);
 
-    if (HAL_SPI_Transmit(hspi, header, 3, HAL_MAX_DELAY) != HAL_OK) {
-        CSHigh();
-        return false;
-    }
-
-    if (HAL_SPI_Receive(hspi, data, len, HAL_MAX_DELAY) != HAL_OK) {
-        CSHigh();
-        return false;
-    }
-
-    CSHigh();
+    ConfigureRMII_Port3();
     return true;
 }
 
+bool KSZ9893RDriver::LinkUp(uint8_t port) {
+    KSZ9893R_REG::REG reg;
 
-bool KSZ9893RDriver::SPI_Write(uint16_t addr, const uint8_t* data, uint16_t len) {
-    if (!initialized || !hspi || !data || len == 0)
-        return false;
-
-    uint8_t header[2];
-    header[0] = KSZ_SPI_WRITE | ((addr >> 8) & 0x7F);
-    header[1] = addr & 0xFF;
-
-    CSLow();
-
-    if (HAL_SPI_Transmit(hspi, header, 2, HAL_MAX_DELAY) != HAL_OK) {
-        CSHigh();
-        return false;
+    switch (port) {
+        case 1: reg = KSZ9893R_REG::PORT1_STATUS_1; break;
+        case 2: reg = KSZ9893R_REG::PORT2_STATUS_1; break;
+        case 3: reg = KSZ9893R_REG::PORT3_STATUS_1; break;
+        default: return false;
     }
 
-    if (HAL_SPI_Transmit(hspi, (uint8_t*)data, len, HAL_MAX_DELAY) != HAL_OK) {
-        CSHigh();
-        return false;
-    }
-
-    CSHigh();
-    return true;
+    return (ReadReg(reg) & 0x01);
 }
-
-
-uint8_t KSZ9893RDriver::GetRegister(KSZ9893R_REG::REG reg) {
-    uint8_t val = 0;
-
-    if (!SPI_Read(static_cast<uint16_t>(reg), &val, 1))
-        return 0;
-
-    return val;
-}
-
-
-bool KSZ9893RDriver::SetRegister(KSZ9893R_REG::REG reg, uint8_t value) {
-    return SPI_Write(static_cast<uint16_t>(reg), &value, 1);
-}
-
-
-bool KSZ9893RDriver::ReadRegisters(KSZ9893R_REG::REG startReg, uint16_t length, uint8_t* buffer) {
-    return SPI_Read(static_cast<uint16_t>(startReg), buffer, length);
-}
-
-
-bool KSZ9893RDriver::SoftReset() {
-    uint8_t val = GetRegister(KSZ9893R_REG::SWITCH_OP);
-    val |= 0x01;
-    bool ok = SetRegister(KSZ9893R_REG::SWITCH_OP, val);
-    HAL_Delay(100);
-    return ok;
-}
-
-
-bool KSZ9893RDriver::LinkStatus(uint8_t port) {
-    if (port < 1 || port > 3)
-        return false;
-
-    static const KSZ9893R_REG::REG statusRegs[3] = {
-        KSZ9893R_REG::PORT1_STATUS_1,
-        KSZ9893R_REG::PORT2_STATUS_1,
-        KSZ9893R_REG::PORT3_STATUS_1
-    };
-
-    uint8_t status = GetRegister(statusRegs[port - 1]);
-
-
-    return (status & 0x01) != 0;
-}
-
-
-
--- Components/KSZ9893R.hpp
+++ Components/KSZ9893R.hpp
@@ -5,13 +5,19 @@
 #include <stdint.h>
 
 namespace KSZ9893R_REG {
+
     enum REG : uint16_t {
-        CHIP_ID1       = 0x0000,
-        CHIP_ID2       = 0x0001,
-        SWITCH_OP      = 0x0300,
-        PORT1_STATUS_1 = 0x011E,
-        PORT2_STATUS_1 = 0x021E,
-        PORT3_STATUS_1 = 0x031E
+        CHIP_ID1        = 0x0000,
+        CHIP_ID2        = 0x0001,
+        SWITCH_OP       = 0x0300,
+        PORT1_STATUS_1  = 0x011E,
+        PORT2_STATUS_1  = 0x021E,
+        PORT3_STATUS_1  = 0x031E,
+        PORT1_CTRL_1    = 0x0104,
+        PORT2_CTRL_1    = 0x0204,
+        PORT3_CTRL_1    = 0x0304,
+        PORT3_XMII_CTRL = 0x0312,
+        PORT3_CLK_CTRL  = 0x0313
     };
 }
 
@@ -19,28 +25,24 @@ class KSZ9893RDriver {
 public:
     bool Init(SPI_HandleTypeDef* hspi, GPIO_TypeDef* cs_gpio, uint16_t cs_pin);
 
-    uint8_t GetRegister(KSZ9893R_REG::REG reg);
-    bool    SetRegister(KSZ9893R_REG::REG reg, uint8_t value);
-
-    bool ReadRegisters(KSZ9893R_REG::REG startReg, uint16_t length, uint8_t* buffer);
-
+    uint8_t ReadReg(KSZ9893R_REG::REG reg);
+    bool    WriteReg(KSZ9893R_REG::REG reg, uint8_t value);
+    bool ReadRegs(uint16_t startAddr, uint8_t* buffer, uint16_t length);
     bool SoftReset();
-    bool LinkStatus(uint8_t port);
+    bool LinkUp(uint8_t port);
 
 private:
     SPI_HandleTypeDef* hspi = nullptr;
     GPIO_TypeDef* cs_gpio = nullptr;
     uint16_t cs_pin = 0;
-    bool initialized = false;
 
     void CSLow();
     void CSHigh();
 
     bool SPI_Read(uint16_t addr, uint8_t* data, uint16_t len);
     bool SPI_Write(uint16_t addr, const uint8_t* data, uint16_t len);
+
+    bool ConfigureRMII_Port3();
 };
 
 #endif
-
-
-
