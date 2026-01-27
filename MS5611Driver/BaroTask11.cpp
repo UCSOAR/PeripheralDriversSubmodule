6ec18938bf3ca722d44ccf7cbaa148cffe78aa8e
Jad Dina <jad.dina19@gmail.com>
2026-01-26 22:28:02 -0700

    added Baro2 task

---
+++ MS5611Driver/BaroTask11.cpp
@@ -0,0 +1,117 @@
+/*
+ * BaroTask11.cpp
+ *
+ *  Created on: Jan 26, 2026
+ *      Author: jaddina
+ */
+
+
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include "BaroTask11.hpp"
+#include "SystemDefines.hpp"
+#include "Command.hpp"
+#include "LoggingService.hpp"
+
+
+#include "DataBroker.hpp"
+#include "Task.hpp"
+/*
+#include "WriteBufferFixedSize.h"
+#include "ReadBufferFixedSize.h"
+#include "cobs.h"
+*/
+/************************************
+ * PRIVATE MACROS AND DEFINES
+ ************************************/
+
+/************************************
+ * VARIABLES
+ ************************************/
+
+/************************************
+ * FUNCTION DECLARATIONS
+ ************************************/
+
+
+
+/************************************
+ * FUNCTION DEFINITIONS
+ ************************************/
+BaroTask11::BaroTask11():Task(TASK_LOGGING_QUEUE_DEPTH_OBJS)
+{
+
+}
+
+/**
+ * @brief Initialize the BaroTask11
+ *        Do not modify this function aside from adding the task name
+ */
+void BaroTask11::InitTask()
+{
+    // Make sure the task is not already initialized
+    SOAR_ASSERT(rtTaskHandle == nullptr, "Cannot initialize watchdog task twice");
+
+    BaseType_t rtValue =
+        xTaskCreate((TaskFunction_t)BaroTask11::RunTask,
+            (const char*)"BaroTask11",
+            (uint16_t)TASK_LOGGING_QUEUE_DEPTH_WORDS,
+            (void*)this,
+            (UBaseType_t)TASK_LOGGING_PRIORITY,
+            (TaskHandle_t*)&rtTaskHandle);
+
+                SOAR_ASSERT(rtValue == pdPASS, "BaroTask11::InitTask() - xTaskCreate() failed");
+}
+
+void BaroTask11::Run(void * pvParams){
+
+	barometer = MS5607Driver(hspi, MS5607_CS_PORT, MS5607_CS_PIN);
+
+    while (1) {
+        /* Process commands in blocking mode */
+        Command cm;
+        bool res = qEvtQueue->ReceiveWait(cm);
+        if(res){
+
+        	HandleCommand(cm);
+        }
+    }
+}
+
+void BaroTask11::HandleCommand(Command& cm){
+	switch(cm.getCommand()){
+	case DATA_COMMAND:
+		HandleRequestCommand(cm.GetTaskCommand());
+		break;
+
+	case TASK_SPECIFIC_COMMAND:
+		break;
+	}
+
+
+
+}
+void BaroTask11::HandleRequestCommand(uint16_t taskCommand){
+	switch(taskCommand){
+	case BARO_SAMPLE_AND_LOG:
+		data = barometer.getSample();
+		LogData();
+	default:
+		break;
+	}
+
+
+}
+
+void BaroTask11::LogData(){
+	DataBroker::Publish<BaroData>(&data);
+	Command logCommand(DATA_BROKER_COMMAND, DataBrokerMessageTypes::BARO_DATA);
+	LoggingTask::Inst().GetEventQueue()->Send(flashCommand);
+
+}
+
+
+
+
---
+++ MS5611Driver/Inc/BaroTask11.hpp
@@ -0,0 +1,71 @@
+/*
+ * BaroTask11.hpp
+ *
+ *  Created on: Jan 26, 2026
+ *      Author: jaddina
+ */
+
+#ifndef PERIPHERALDRIVERSSUBMODULE_MS5607DRIVER_INC_BAROTASK11_HPP_
+#define PERIPHERALDRIVERSSUBMODULE_MS5607DRIVER_INC_BAROTASK11_HPP_
+
+#include "Task.hpp"
+#include "MS5611Driver.hpp"
+/************************************
+ * MACROS AND DEFINES
+ ************************************/
+
+
+/************************************
+ * TYPEDEFS
+ ************************************/
+
+/************************************
+ * CLASS DEFINITIONS
+ ************************************/
+
+/************************************
+ * FUNCTION DECLARATIONS
+ ************************************/
+class BaroTask11: public Task
+{
+	public:
+		static BaroTask11& Inst() {
+			static BaroTask11 inst;
+			return inst;
+		}
+
+		void InitTask();
+
+
+
+	protected:
+		static void RunTask(void* pvParams) { BaroTask11::Inst().Run(pvParams); } // Static Task Interface, passes control to the instance Run();
+		void Run(void * pvParams); // Main run code
+		void HandleCommand(Command& cm);
+		void HandleRequestCommand(uint16_t taskCommand);
+		BaroData data;
+		MS5607Driver barometer;
+		GPIO_TypeDef* MS5611_CS_PORT = GPIOA;
+		const uint16_t MS5611_CS_PIN = GPIO_PIN_4; //adjust when needed
+		SPI_HandleTypeDef* hspi_ = 4;// adjust this when needed
+
+
+
+
+
+	private:
+		// Private Functions
+		BaroTask11();        // Private constructor
+		BaroTask11(const BaroTask11&);                        // Prevent copy-construction
+		BaroTask11& operator=(const BaroTask11&);														// Prevent assignment
+		void LogData();
+};
+
+
+
+
+
+
+
+
+#endif /* PERIPHERALDRIVERSSUBMODULE_MS5607DRIVER_INC_BAROTASK11_HPP_ */
