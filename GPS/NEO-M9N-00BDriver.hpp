4fccd644ce78b4405586f8fe4f620112d63328f3
Jad Dina <jad.dina19@gmail.com>
2026-03-28 15:52:31 -0600

    finished basic functions for GPS Driver

--- GPS/NEO-M9N-00BDriver.hpp
+++ GPS/NEO-M9N-00BDriver.hpp
@@ -10,6 +10,35 @@
 #define CFG_SPIOUTPROT_NMEA 0x107a0002 //KeyID to indicate if NMEA is output on SPI
 #define CFG_SPIINPROT_NMEA 0x10790002 //KeyID to indicate if NMEA is input on SPI
 #define CFG_SPIINPROT_UBX 0x10790001 //KeyID to indicate if UBX is input on SPI
+#define CFG_SPIOUTPROT_UBX 0x107a0001 //KeyID to indicate if UBX is output on SPI
+
+#define CFG_MSGOUT_NMEA_ID_GGA_SPI  0x209100be //KeyID to specify output rate of GGA on SPI
+
+#define NMEA_MAX_LENGTH 82 //Max length for NMEA message including start and end characters
+
+typedef struct
+{
+    int32_t    degrees_;
+    int32_t    minutes_;
+} LatLongType;
+
+typedef struct
+{
+    int32_t     altitude_;
+    char        unit_;
+} AltitudeType;
+
+typedef struct
+{
+    char            buffer_ [NMEA_MAX_LENGTH];
+    uint32_t        time_;
+    LatLongType     latitude_;
+    LatLongType     longitude_;
+    AltitudeType    antennaAltitude_;
+    AltitudeType    geoidAltitude_;
+    AltitudeType    totalAltitude_;
+} GpsData;
+
 
 enum class NEOM9N00B_Status{
 	OK = 0
@@ -26,9 +55,14 @@ public:
 	NEOM9N00B_Status Init(SPI_HandleTypeDef* hspi, GPIO_TypeDef* csPort, uint16_t csPin);
 
 private:
-	void valSetLType(uint32_t keyID, uint8_t value)
+	bool valSetLType(uint32_t keyID, uint8_t value)
+	bool valSetU1Type(uint32_t keyID, uint8_t value)
 
-	void NEOM9N00B::computeUBXChecksum(const uint8_t* data, uint16_t len, uint8_t& ckA, uint8_t& ckB);
+	bool computeUBXChecksum(const uint8_t* data, uint16_t len, uint8_t& ckA, uint8_t& ckB);
+	bool waitForAck(uint8_t expectedClass, uint8_t expectedId, uint32_t timeoutMs);
+	bool collectNMEALine(char* lineBuf)
+	bool readBytes(uint8_t* rxBuf, uint16_t len)
+	bool getGGALine(char* lineBuf)
 
 	void csHigh();
 	void csLow();
