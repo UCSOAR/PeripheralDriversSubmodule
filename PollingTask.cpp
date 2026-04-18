5f5a0332ee2009c8ada7255401a51bf110489fde
Jad Dina <jad.dina19@gmail.com>
2026-04-18 12:16:49 -0600

    added polling task

---
+++ PollingTask.cpp
@@ -0,0 +1,251 @@
+/*
+ * PollingTask.cpp
+ *
+ *  Created on: Apr 8, 2026
+ *      Author: jaddina
+ */
+
+#include "PollingTask.hpp"
+#include "FreeRTOS.h"
+#include "timers.h"
+#include "DataBroker.hpp"
+
+namespace {
+constexpr uint32_t kLaunchPollMs = 20;
+constexpr uint32_t kBoostPollMs = 20;
+constexpr uint32_t kCoastPollMs = 100;
+constexpr uint32_t kDescentPollMs = 100;
+constexpr uint32_t kRecoveryPollMs = 250;
+}
+
+PollingTask::PollingTask():Task(TASK_LOGGING_QUEUE_DEPTH_OBJS), pollTimerHandle(nullptr), flightState(FlightState::Grounded), imu16(), imu32(), magnetometer()
+{
+
+}
+
+/**
+ * @brief Initialize the PollingTask
+ *        Do not modify this function aside from adding the task name
+ */
+void PollingTask::InitTask()
+{
+    // Make sure the task is not already initialized
+    SOAR_ASSERT(rtTaskHandle == nullptr, "Cannot initialize watchdog task twice");
+
+    BaseType_t rtValue =
+        xTaskCreate((TaskFunction_t)PollingTask::RunTask,
+            (const char*)"PollingTask",
+            (uint16_t)TASK_LOGGING_QUEUE_DEPTH_WORDS,
+            (void*)this,
+            (UBaseType_t)TASK_LOGGING_PRIORITY,
+            (TaskHandle_t*)&rtTaskHandle);
+
+                SOAR_ASSERT(rtValue == pdPASS, "PollingTask::InitTask() - xTaskCreate() failed");
+
+    //Init drivers
+	imu16.Init(hspi6_, LSM6DSO_CS_PIN,  LSM6DSO_CS_PORT );
+	imu32.Init(hspi2_, LSM6DSO32_CS_PORT, LSM6DSO32_CS_PIN);
+	magnetometer.Init(hspi4_, MMC_CS_PORT, MAG_CS_Pin);
+
+	pollTimerHandle = xTimerCreate("PollingTimer",
+			pdMS_TO_TICKS(kLaunchPollMs),
+			pdFALSE,
+			(void*)this,
+			PollingTask::PollTimerCallback);
+	SOAR_ASSERT(pollTimerHandle != nullptr, "PollingTask::InitTask() - xTimerCreate() failed");
+
+	ApplyFlightState();
+}
+
+void PollingTask::SetFlightState(FlightState newState)
+{
+	flightState = newState;
+	ApplyFlightState();
+}
+
+uint32_t PollingTask::GetPollingPeriodMs(FlightState state)
+{
+	switch (state)
+	{
+	case FlightState::Grounded:
+	case FlightState::Landed:
+		return 0;
+	case FlightState::Launch:
+		return kLaunchPollMs;
+	case FlightState::Boost:
+		return kBoostPollMs;
+	case FlightState::Coast:
+		return kCoastPollMs;
+	case FlightState::Descent:
+		return kDescentPollMs;
+	case FlightState::Recovery:
+		return kRecoveryPollMs;
+	default:
+		return 0;
+	}
+}
+
+void PollingTask::ApplyFlightState()
+{
+	if (pollTimerHandle == nullptr)
+	{
+		return;
+	}
+
+	const uint32_t pollPeriodMs = GetPollingPeriodMs(flightState);
+	if (pollPeriodMs == 0)
+	{
+		(void)xTimerStop(pollTimerHandle, 0);
+		return;
+	}
+
+	(void)xTimerChangePeriod(pollTimerHandle, pdMS_TO_TICKS(pollPeriodMs), 0);
+}
+
+void PollingTask::Run(void * pvParams){
+    while (1) {
+        Command cm;
+        bool res = qEvtQueue->ReceiveWait(cm);
+        if(res){
+
+        	HandleCommand(cm);
+        }
+    }
+}
+
+void PollingTask::HandleCommand(Command& cm){
+	switch(cm.GetCommand()){
+	case DATA_COMMAND:
+		HandleRequestCommand(cm.GetTaskCommand());
+		break;
+
+	case TASK_SPECIFIC_COMMAND:
+		break;
+	}
+
+	cm.Reset();
+
+}
+
+void PollingTask::HandleRequestCommand(uint16_t taskCommand){
+	switch(taskCommand){
+	case POLL_SENSORS_AND_LOG:
+		PollSensors();
+		LogData();
+		break;
+
+	default:
+		break;
+	}
+}
+
+void PollingTask::PollTimerCallback(TimerHandle_t xTimer)
+{
+	PollingTask* task = static_cast<PollingTask*>(pvTimerGetTimerID(xTimer));
+	if ((task == nullptr) || (task->GetEventQueue() == nullptr)) {
+		return;
+	}
+
+	Command cmd(DATA_COMMAND, PollingTask::POLL_SENSORS_AND_LOG);
+	task->GetEventQueue()->Send(cmd, false);
+	task->ApplyFlightState();
+}
+
+
+void PollingTask::PollSensors()
+{
+	uint8_t data[14] = {0};
+
+	baro07Data = barometer07.getSample();
+	baro07Data.id = 0;
+
+	baro11Data = barometer11.getSample();
+	baro11Data.id = 1;
+
+	imu16.readSensors(data);
+	imu16Data = imu16.bytesToStruct(data, true, true, true);
+	imu16Data.id = 0;
+
+	imu32.ReadSensors(data);
+	imu32Data = imu32.ConvertRawMeasurementToStruct(data);
+	imu32Data.id = 1;
+
+	magnetometer.triggerMeasurement();
+	magnetometer.readData(driverData);
+
+	magData.magX = driverData.scaledX;
+	magData.magY = driverData.scaledY;
+	magData.magZ = driverData.scaledZ;
+}
+
+
+void PollingTask::LogData(){
+	TickType_t nowTick = xTaskGetTickCount();
+
+	if(!pollingTimerStarted){
+		pollingStartTick = nowTick;
+		previousLogTick = nowTick;
+		pollingTimerStarted = true;
+	}
+
+	TickType_t elapsedTicks = nowTick - pollingStartTick;
+	TickType_t deltaTicks   = nowTick - previousLogTick;
+
+	float elapsedSec = (float)elapsedTicks / configTICK_RATE_HZ;
+	float deltaSec   = (float)deltaTicks / configTICK_RATE_HZ;
+	uint32_t rateHz     = (deltaSec > 0.0f) ? (1.0f / deltaSec) : 0.0f;
+
+	uint32_t elapsedMs = (elapsedTicks * 1000U) / configTICK_RATE_HZ;
+	uint32_t deltaMs   = (deltaTicks * 1000U) / configTICK_RATE_HZ;
+
+	SOAR_PRINT("PollingTask Timing | elapsed=%d ms dt=%d ms rate=%d Hz\n",
+			   elapsedMs,
+			   deltaMs,
+			   rateHz);
+
+	previousLogTick = nowTick;
+
+	SOAR_PRINT("Log data polled");
+//	SOAR_PRINT("PollingTask Log | Baro07 id=%u temp=%d pressure=%lu\n",
+//			(unsigned int)baro07Data.id,
+//			(int)baro07Data.temp,
+//			(unsigned long)baro07Data.pressure);
+//	SOAR_PRINT("PollingTask Log | Baro11 id=%u temp=%d pressure=%lu\n",
+//			(unsigned int)baro11Data.id,
+//			(int)baro11Data.temp,
+//			(unsigned long)baro11Data.pressure);
+//
+//	SOAR_PRINT("PollingTask Log | IMU16 id=%u accel=(%d,%d,%d) gyro=(%d,%d,%d) temp=%d\n",
+//			(unsigned int)imu16Data.id,
+//			(int)imu16Data.accel.x,
+//			(int)imu16Data.accel.y,
+//			(int)imu16Data.accel.z,
+//			(int)imu16Data.gyro.x,
+//			(int)imu16Data.gyro.y,
+//			(int)imu16Data.gyro.z,
+//			(int)imu16Data.temp);
+//
+//	SOAR_PRINT("PollingTask Log | IMU32 id=%u accel=(%d,%d,%d) gyro=(%d,%d,%d) temp=%d\n",
+//			(unsigned int)imu32Data.id,
+//			(int)imu32Data.accel.x,
+//			(int)imu32Data.accel.y,
+//			(int)imu32Data.accel.z,
+//			(int)imu32Data.gyro.x,
+//			(int)imu32Data.gyro.y,
+//			(int)imu32Data.gyro.z,
+//			(int)imu32Data.temp);
+//
+//	SOAR_PRINT("PollingTask Log | MAG scaled=(%ld,%ld,%ld)\n",
+//			(long)magData.magX,
+//			(long)magData.magY,
+//			(long)magData.magZ);
+
+	DataBroker::Publish<IMUData>(&imu16Data);
+	DataBroker::Publish<IMUData>(&imu32Data);
+	DataBroker::Publish<BaroData>(&baro07Data);
+	DataBroker::Publish<BaroData>(&baro11Data);
+	DataBroker::Publish<MagData>(&magData);
+
+
+
+}
---
+++ PollingTask.hpp
@@ -0,0 +1,129 @@
+/*
+ * PollingTask.hpp
+ *
+ *  Created on: Apr 8, 2026
+ *      Author: jaddina
+ */
+
+#include "Task.hpp"
+#include "MS5607Driver.hpp"
+#include "MS5611Driver.hpp"
+#include "lsm6dso.hpp"
+#include <LSM6DO32Driver.h>
+#include "mmc5983ma.hpp"
+
+#include "SensorDataTypes.hpp"
+#include "main.h"
+
+extern SPI_HandleTypeDef hspi1;
+extern SPI_HandleTypeDef hspi2;
+extern SPI_HandleTypeDef hspi3;
+extern SPI_HandleTypeDef hspi4;
+extern SPI_HandleTypeDef hspi5;
+extern SPI_HandleTypeDef hspi6;
+
+
+#ifndef COMPONENTS_POLLINGTASK_HPP_
+#define COMPONENTS_POLLINGTASK_HPP_
+
+class PollingTask: public Task
+{
+	public:
+		enum class FlightState : uint8_t {
+			Grounded = 0,
+			Launch,
+			Boost,
+			Coast,
+			Descent,
+			Recovery,
+			Landed,
+		};
+
+		enum PollingTaskCommands : uint16_t {
+			POLL_SENSORS_AND_LOG = 1,
+		};
+
+		static PollingTask& Inst() {
+			static PollingTask inst;
+			return inst;
+		}
+
+		void InitTask();
+		void SetFlightState(FlightState newState);
+		FlightState GetFlightState() const { return flightState; }
+
+
+
+	protected:
+
+		static void RunTask(void* pvParams) { PollingTask::Inst().Run(pvParams); } // Static Task Interface, passes control to the instance Run();
+		void Run(void * pvParams); // Main run code
+		void HandleCommand(Command& cm);
+		void HandleRequestCommand(uint16_t taskCommand);
+		static void PollTimerCallback(TimerHandle_t xTimer);
+
+
+
+	private:
+		// Private Functions
+		PollingTask();        // Private constructor
+		PollingTask(const PollingTask&);                        // Prevent copy-construction
+		PollingTask& operator=(const PollingTask&);				// Prevent assignment
+		void LogData();
+		void PollSensors();
+		void ApplyFlightState();
+		static uint32_t GetPollingPeriodMs(FlightState state);
+		TimerHandle_t pollTimerHandle;
+
+		//to count time elapsed and time taken per poll and Hz
+		TickType_t pollingStartTick = 0;
+		TickType_t previousLogTick = 0;
+		bool pollingTimerStarted = false;
+
+		FlightState flightState;
+		//sensor data structs
+		BaroData baro07Data;
+		BaroData baro11Data;
+		IMUData imu32Data;
+		IMUData imu16Data;
+		MagData magData;
+		MagDriverData driverData;
+
+		//All sensor gpios and spi
+		//Baro 07 config
+		GPIO_TypeDef* MS5607_CS_PORT = BARO07_CS_GPIO_Port;
+		const uint16_t MS5607_CS_PIN = BARO07_CS_Pin;
+		SPI_HandleTypeDef* hspi3_= &hspi3;
+
+		//Baro 11 config
+		GPIO_TypeDef* MS5611_CS_PORT = BARO11_CS_GPIO_Port;
+		const uint16_t MS5611_CS_PIN = BARO11_CS_Pin;
+		SPI_HandleTypeDef* hspi1_ = &hspi1;
+
+		//IMU 32 config
+		GPIO_TypeDef *LSM6DSO32_CS_PORT = IMU32_CS_GPIO_Port;
+		const uint16_t LSM6DSO32_CS_PIN = IMU32_CS_Pin;
+		SPI_HandleTypeDef *hspi2_ = &hspi2;
+
+		//IMU 16 config
+		GPIO_TypeDef *LSM6DSO_CS_PORT = IMU16_CS_GPIO_Port;
+		const uint16_t LSM6DSO_CS_PIN = IMU16_CS_Pin;
+		SPI_HandleTypeDef *hspi6_ = &hspi6;
+
+		//Mag config
+	    SPI_HandleTypeDef *hspi4_ = &hspi4;
+	    GPIO_TypeDef *MMC_CS_PORT = MAG_CS_GPIO_Port;
+	    const uint16_t MMC_CS_PIN = MAG_CS_Pin;
+
+	    //Drivers
+	    LSM6DSO_Driver imu16;
+	    LSM6DO32_Driver imu32;
+	    MS5607_Driver barometer07{hspi3_, MS5607_CS_PORT, MS5607_CS_PIN};
+	    MS5611_Driver barometer11{hspi1_, MS5611_CS_PORT, MS5611_CS_PIN};
+	    MMC5983MA magnetometer;
+
+};
+
+
+
+#endif /* COMPONENTS_POLLINGTASK_HPP_ */
