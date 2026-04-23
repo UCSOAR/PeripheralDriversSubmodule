97a05e0a33cd01ff0c4a34c8f9e6540a963cce21
Adam <145816785+b-atteryacid@users.noreply.github.com>
2026-04-22 20:13:52 -0600

    Adamg/fdcanh7 (#32)
    Moved H7 FDCAN base-level driver to its own folder, tested on the board
    stack at 250kbit/s as the base driver for canautonode, and renamed some
    things.

---
+++ FDCanH7/FDCan.h
@@ -0,0 +1,136 @@
+/*
+ * FDCan.h
+ *
+ *  Created on: Aug 24, 2024
+ *      Author: goada
+ */
+
+#ifndef FDCAN_H_
+#define FDCAN_H_
+
+#include "stm32h7xx.h"
+
+// *********************** DEBUG:
+//#define FDCAN_DEBUG
+//#define FDCAN_DEBUG_VERBOSE
+
+constexpr size_t MAX_FDCAN_RX_BUFFERS = 128;
+constexpr size_t MAX_FDCAN_LOGS = 8;
+
+
+class FDCanController {
+ public:
+  struct LogInitStruct {
+    uint16_t byteLength = 0;
+    uint16_t startingMsgID = 0;
+  };
+
+  FDCanController(FDCAN_HandleTypeDef *fdcan,
+                  FDCanController::LogInitStruct *logs, uint16_t numLogs);
+  ~FDCanController();
+  FDCanController(const FDCanController &) = delete;
+  FDCanController &operator=(const FDCanController &) = delete;
+
+  bool SendByLogIndex(const uint8_t *msg, uint16_t logIndex);
+
+  bool SendStringByLogIndex(const char *msg, uint16_t logIndex);
+
+  bool SendByMsgID(const uint8_t *msg, size_t len, uint16_t ID, uint16_t timeout = 1000);
+
+  bool AddLogType(LogInitStruct log);
+  bool RemoveLogIndices(const uint8_t* indices, uint8_t count);
+
+  HAL_StatusTypeDef RegisterLogs(LogInitStruct *logs, uint16_t numLogs);
+
+  uint16_t ReceiveFirstLogFromRXBuf(uint8_t *out, uint16_t *msgID);
+  uint16_t ReceiveLogIndexFromRXBuf(uint8_t *out, uint16_t logIndexFilter);
+
+  static uint16_t FDRoundDataSize(uint16_t unroundedLen);
+  static uint32_t FDGetModDLC(uint16_t unroundedLen);
+  static uint16_t FDGetByteLengthOfDLC(uint16_t DLC);
+
+  inline void RaiseFXFlag();
+
+  HAL_StatusTypeDef GetRxFIFO(uint8_t* out, uint32_t* msgIDOut);
+
+  struct RXBuffer {
+	  uint8_t data[64];
+	  volatile bool available = false;
+  };
+
+  enum SelectedBuffer {
+	  Buf_A,
+	  Buf_B
+  };
+  SelectedBuffer selectedBufsForLog[MAX_FDCAN_LOGS];
+
+  RXBuffer* GetRXBuf(uint16_t index,SelectedBuffer side);
+
+  RXBuffer* GetFrontBufferFromCanID(uint16_t canid);
+  RXBuffer* GetBackBufferFromCanID(uint16_t canid);
+  HAL_StatusTypeDef GetRxMessageDirect(
+          FDCAN_RxHeaderTypeDef *pRxHeader);
+
+  bool DiscardLog(uint16_t index);
+
+#ifdef FDCAN_DEBUG
+  void ReportIDsReceived();
+  uint32_t GetTicksSinceLastIDReceivePrint();
+#endif
+
+ protected:
+  struct LogRegister {
+    uint8_t startingRXBuf = 0;
+    uint8_t endingRXBuf = 0;
+    uint16_t byteLength = 0;
+    uint16_t startingMsgID = 0;
+  };
+
+
+
+
+  bool RebuildFilters();
+
+  RXBuffer buffersA[MAX_FDCAN_RX_BUFFERS];
+  RXBuffer buffersB[MAX_FDCAN_RX_BUFFERS];
+
+  struct BufferTracker {
+	  RXBuffer* A;
+	  RXBuffer* B;
+	  SelectedBuffer* logOwnerSelection;
+  };
+
+  BufferTracker buffersByCanID[2048];
+
+
+
+  FDCAN_HandleTypeDef *fdcan;
+
+  uint8_t numFDFilters = 0;
+
+  uint8_t nextUnregisteredFilterID = 0;
+  uint8_t numRegisteredLogs = 0;
+  LogRegister registeredLogs[MAX_FDCAN_LOGS];
+
+  HAL_StatusTypeDef RegisterFilterRXBuf(uint16_t startingID, uint16_t endingID);
+
+#ifdef FDCAN_DEBUG
+
+  uint16_t idReceiveTracker[2048];
+  uint32_t lastReceivePrintTick = 0;
+
+#endif
+
+  // Raised when there is at least 1 rx msg
+  volatile bool RXFlag = false;
+
+  volatile bool readingRXBufSemaphore = false;
+
+
+};
+
+extern FDCanController *callbackcontroller;
+
+void CANError();
+
+#endif /* FDCAN_H_ */
